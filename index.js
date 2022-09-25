6d7e1d14609079da99d2441d9d9991cafc00d42e
JabedH <jabedhtusar@gmail.com>
2022-09-25 17:40:57 +0600

    conected DB

--- index.js
+++ index.js
@@ -7,6 +7,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// mongodb
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://sosc-assignment:mgNDoAXwcnPsfNgY@cluster0.beisjei.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+// CRUD
+async function run() {
+  try {
+    await client.connect();
+    app.post("/saveUser", async, (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server-side is working fine");
+});
+
 app.listen(port, () => {
-  console.log("server is ", port);
+  console.log("server is running", port);
 });
