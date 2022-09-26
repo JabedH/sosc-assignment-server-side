737abfc10f1a8d2fa0d87fac36bfec6851224df2
JabedH <jabedhtusar@gmail.com>
2022-09-26 13:47:49 +0600

    finally deploy

--- index.js
+++ index.js
@@ -9,25 +9,20 @@ app.use(express.json());
 
 // mongodb
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://sosc-assignment:mgNDoAXwcnPsfNgY@cluster0.beisjei.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.beisjei.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-// client.connect((err) => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
-// CRUD
 async function run() {
   try {
     await client.connect();
     const userCollection = client.db("userData").collection("users");
+    const quotesCollection = client.db("quotesData").collection("quotes");
 
+    // save new user
     app.put("/saveUser/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
@@ -40,18 +35,30 @@ async function run() {
 
       res.send({ result });
     });
+
+    //post data
+    app.post("/quotes", async (req, res) => {
+      const data = req.body;
+      const result = await quotesCollection.insertOne(data);
+      res.send(result);
+    });
+    app.get("/quotes", async (req, res) => {
+      const users = await quotesCollection.find().toArray();
+      res.send(users);
+    });
+
+    // get user
     app.get("/allUsers", async (req, res) => {
       const email = req.query.email;
-      // const email = req.params.email;
       const query = { email: email };
-      const booking = await userCollection.find(query).toArray();
-      res.send(booking);
+      const users = await userCollection.find(query).toArray();
+      res.send(users);
     });
     // update all user
     app.patch("/allUsers/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
-      console.log(user);
+
       const filter = { email: email };
       const options = { upsert: true };
       const updateDoc = {
